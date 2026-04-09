49b27fb71825c85c36ec43b6328ecdc911a9ebe4
Smirti-Shahi <shahismriti2004@gmail.com>
2026-04-09 21:56:19 +0545

    user dao implement

--- src/main/java/com/studentEnrollment/dao/UserDAO.java
+++ src/main/java/com/studentEnrollment/dao/UserDAO.java
@@ -1,21 +1,100 @@
 package com.studentEnrollment.dao;
 
+import com.studentEnrollment.model.User;
 import com.studentEnrollment.util.DbConfig;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UserDAO {
 
-    public boolean registerUser(){
+    public boolean registerUser(User user){
         String sql ="Insert into users (fullname, email, password,role) values (?,?,?,?)";
         try(Connection conn = DbConfig.getDbConnection();
             PreparedStatement ps = conn.prepareStatement(sql)
         ){
+            ps.setString(1, user.getFullName());
+            ps.setString(2,user.getEmail());
+            ps.setString(3,user.getPassword());
+            ps.setString(3,user.getRole());
+            return ps.executeUpdate() >0;
 
 
         } catch (Exception e) {
-            return false;
+            System.out.println("ERror registering user: "+e.getMessage());
         }
+        return false;
+
     }
+
+    public User loginUser(String email, String password){
+        String sql = "Select * from users where email =? AND password =?";
+        try(Connection conn =DbConfig.getDbConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)
+        ){
+            ps.setString(1,email);
+            ps.setString(2,password);
+            ResultSet rs =ps.executeQuery();
+            if(rs.next()){
+                return new User(rs.getInt("id"),rs.getString("fullname"),
+                        rs.getString("email"),rs.getString("password"),
+                        rs.getString("role"));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error during loin: "+e.getMessage());
+        }
+        return null;
+    }
+
+    public User findByEmail(String email){
+        String sql = "Select * from users where email =?";
+        try(Connection conn = DbConfig.getDbConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)
+        ){
+            ps.setString(1,email);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                return new User(
+                        rs.getInt("id"),
+                        rs.getString("fullname"),
+                        rs.getString("email"),
+                        rs.getString("password"),
+                        rs.getString("role")
+                );
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error finding user by email"+e.getMessage());
+        }
+        return null;
+    }
+
+    public User findById(int id){
+        String sql = "Select * from users where id =?";
+        try(Connection conn = DbConfig.getDbConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)
+        ){
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                return new User(
+                        rs.getInt("id"),
+                        rs.getString("fullname"),
+                        rs.getString("email"),
+                        rs.getString("password"),
+                        rs.getString("role")
+                );
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error finding user by id: "+e.getMessage());
+
+        }
+        return null;
+    }
+
+
+
 }
